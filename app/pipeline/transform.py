dc0e214e9b5e0611bcc56fd57eecd0838f7cabff
mslbr-vgabriel <vgabriel@mslcorporate.com.br>
2024-02-09 17:42:57 -0300

    modulo load adicionado

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -6,4 +6,4 @@ função para transformar uma lista de dataframes em um único dataframe
 """
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
-    return pd.contact(data_frame_list, ignore_index=True)
+    return pd.concat(data_frame_list, ignore_index=True)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,7 +4,7 @@ from app.pipeline.transform import concat_data_frames
 df_1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
 df_2 = pd.DataFrame({'col1': [5, 6], 'col2': [7, 8]})
 
-def testar_a_concatenacao_da_lista_de_dataframes():
+def test_concat_dataframelist():
     # arrange
     data_frame_list = [df_1, df_2]
 
