2cb2062148bff857c0b7b3195851631738fe9a1c
mslbr-vgabriel <vgabriel@mslcorporate.com.br>
2024-02-14 11:10:27 -0300

    docs

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -2,10 +2,9 @@ from typing import List
 
 import pandas as pd
 
-"""
-função para transformar uma lista de dataframes em um único dataframe
-"""
-
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    função para transformar uma lista de dataframes em um único dataframe
+    """
     return pd.concat(data_frame_list, ignore_index=True)
